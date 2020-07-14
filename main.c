0e1bfb4bc1f1c50b350117d2f8352ffbfe2f65ae
Daniel <danielminami@hotmail.com>
2020-07-14 10:23:59 -0400

    Init

---
+++ main.c
@@ -0,0 +1,23 @@
+/*
+Daniel Minami <minamid@sheridancollege.ca>
+----------------------------------------------------------------------
+ */
+
+/* 
+ * File:   main.c
+ * Author: Daniel Minami <minamid@sheridancollege.ca>
+ *
+ * Created on July 14, 2020, 10:23 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
